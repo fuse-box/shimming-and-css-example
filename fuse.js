5dee9cfe00035bf1e974b690540d7799d8369e33
Ivan.Orlov <ivan@morrr.com>
2017-02-01 09:09:27 +0100

    Initial

---
+++ fuse.js
@@ -0,0 +1,20 @@
+const fsbx = require("fuse-box");
+const FuseBox = fsbx.FuseBox;
+
+const fuseBox = FuseBox.init({
+    homeDir: "src",
+    outFile: "dist/bundle.js",
+    shim: {
+        jquery: {
+            exports: "$",
+            source: "node_modules/jquery/dist/jquery.min.js"
+        }
+    },
+    plugins: [
+        [/node_modules.*\.css$/, fsbx.CSSResourcePlugin(), fsbx.CSSPlugin()]
+    ]
+});
+
+fuseBox.devServer(">index.ts", {
+    port: 7775
+});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "jstree/dist/jstree.js";
+import "jstree/dist/themes/default/style.css";
+
+$(function () {
+    $('#container').jstree();
+});
